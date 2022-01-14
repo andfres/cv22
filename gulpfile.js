6a66757be0cedab761ccbac52063cd19c4107b6c
andfres <andfres@gmail.com>
2022-01-14 13:39:39 +0100

    primer commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const nunjucks = require('gulp-nunjucks');
+
+
+gulp.task('sass', function(){
+
+    console.log("ejecutando sass")
+
+    return gulp.src('src/**/*.scss' , {base: './src'})
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('docs/'));
+
+});
+
+gulp.task('nunjucks', function(){
+
+    console.log("ejecutando nunjucks")
+    
+    return gulp.src(['src/**/*.njk' ,'!src/build/**/*'], {base: './src'})
+        .pipe(nunjucks.compile())
+        .pipe(gulp.dest('docs/'));
+
+});
+
+
+gulp.task('all:watch', function(){
+    gulp.watch('src/**/*.scss',  gulp.series('sass')),
+    gulp.watch([  'src/**/*.njk' ] , gulp.series('nunjucks'));
+    return;
+
+});
+
+
+
+
+
+
